3d587f18c42e954b1e8f7c53dd4f2d0ce70bc290
hnphat <hnphat0415@gmail.com>
2026-06-15 14:53:30 +0700

    feat: add function prepareTest

--- Solution/dictionary_sampler.h
+++ Solution/dictionary_sampler.h
@@ -5,5 +5,6 @@
 #include <string>
 
 std::vector<std::string> sampleDictionary(const std::vector<std::string> &dictionary, int K, unsigned int seed);
+std::string prepareText(const std::string &filename, int N);
 
 #endif // DICTIONARY_SAMPLER_H
